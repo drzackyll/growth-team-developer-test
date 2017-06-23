025bd96a61d570bae6b417c62f31884c3dc43973
drzackyll <adams.z.d@gmail.com>
2017-06-23 15:36:24 -0400

    Functionality complete

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,14 @@
 class StaticPagesController < ApplicationController
 
   def root
+    if !!params["term"]
+      term = params["term"]
+      response = Gems.search(term).first
+      @response_name = response["name"]
+      @response_info = response["info"]
+      @response_url = response["project_uri"]
+      @response_dependencies = response["dependencies"]["runtime"]
+    end
   end
 
   def favorites
